4a562e458b0ceaf08cbda457595f400eb3eb9787
Ifeoma Chukwuyenum <ifydevops23@gmail.com>
2023-08-16 21:04:35 +0000

    c-program files

---
+++ C-PROGRAM/input.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+
+    
+    int age;
+    printf("Enter input value: \n");
+    
+    scanf("%d", &age);
+
+    printf("Age = %d\n" , age);
+    
+    
+    return 0;
+}
---
+++ C-PROGRAM/variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+
+    int age =25;
+   
+    printf("%d \n", age);
+
+    int number =13;
+
+    printf("My number is: %d \n", number);
+    
+    return 0;
+}
